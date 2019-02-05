77f7566f21ed430033e129ac2480173c36166f4f
harryherold <harryherold@googlemail.com>
2019-02-05 10:14:57 +0100

    Move counter creator to project root.

--- otf2_access_stats/create_access_counters.py
+++ otf2_access_stats/create_access_counters.py
@@ -6,8 +6,8 @@ from collections import defaultdict
 import argparse
 import otf2
 
-from spacecollection import AddressSpace, AccessType
-from spacestatistics import MemoryAccessStatistics
+from otf2_access_stats.spacecollection import AddressSpace, AccessType
+from otf2_access_stats.spacestatistics import MemoryAccessStatistics
 
 
 if __name__ == "__main__":
@@ -18,8 +18,8 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.accesses or args.counters:
-        stats = MemoryAccessStatistics()
         with otf2.reader.open(args.trace) as trace_reader:
+            stats = MemoryAccessStatistics(trace_reader.timer_resolution)
             trace_writer = otf2.writer.Writer("rewrite", definitions=trace_reader.definitions)
             for location, event in trace_reader.events:
                 event_writer = trace_writer.event_writer_from_location(location)
@@ -28,7 +28,6 @@ if __name__ == "__main__":
                 if space.initialized():
                     stats.add_mapped_space(space)
                 if isinstance(event, otf2.events.Metric) and AccessType.contains(event.metric.member.name):
-                    stats.add_access(event, location)
             if args.accesses:
                 stats.create_access_metrics(trace_writer)
             if args.counters:
